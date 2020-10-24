58bef628f8c14fcd9011a2117de8a77058424bf3
Shehab <shehabahmed963@gmail.com>
2020-10-24 19:21:09 +0200

    link the css and javaScript files to the HTML file

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function add(a,b){
+	return a+b;
+}
+
+function subtract(a,b){
+	return a-b;
+}
+
+function multiply(a,b){
+	return a*b;
+}
+
+function divide(a,b){
+	return a/b;
+}
+
+console.log(add(1,2))
+console.log(subtract(2,1))
+console.log(multiply(3,2))
+console.log(divide(10,5))
