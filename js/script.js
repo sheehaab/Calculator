a76c53425cf13d3f79b25c229b3e6b15ae353b1a
Shehab <shehabahmed963@gmail.com>
2020-10-29 18:50:21 +0200

    add the dark mode to the calculator and some modification on the style and three new button the percent button and the negtive button and the copy button to copy to clipboard

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@ function multiply(a,b){
 function divide(a,b){
 	return a/b;
 }
+function getPercent(a,b){
+	let y = (a/100) * b;
+	return y;
+}
+
 
 function operate(num1,operator,num2){
 	result = 0;
@@ -27,6 +32,9 @@ function operate(num1,operator,num2){
 	}
 	if(operator === '/'){
 		result = divide(num1,num2);
+	}
+	if(operator === '%'){
+		result = getPercent(num1,num2);
 	}
 	return +result.toFixed(2);
 }
@@ -51,6 +59,10 @@ const minus = document.getElementById('minus');
 const deleteBtn = document.getElementById('delete');
 const allBtns = document.querySelectorAll('button');
 const point = document.getElementById('point');
+const percent = document.getElementById('percent');
+const negative = document.getElementById('negative');
+const copyBtn = document.getElementById('copy');
+const copyMsg = document.getElementById('copy-msg');
 
 let storing = '';
 let firstNum = '';
@@ -61,13 +73,117 @@ let result = '';
 let equalIsPressed = false;
 let pointCounter = 0;
 
+
+
+
+//dark mode 
+
+const toggle = document.getElementById('toggle');
+const calcBackground = document.querySelector('.calc-container');
+const numbersColor = document.querySelector('.numbers');
+const upperBackground = document.querySelector('.upper');
+const lowerNumColor = document.querySelectorAll('.js-style');
+const lowerOpertators = document.querySelectorAll('.lower-operators');
+const lowerOpertatorsCircle = document.querySelectorAll('.back-circle');
+
+toggle.checked = false;
+
+toggle.addEventListener('click',function(){
+	if(toggle.checked === false){
+		calcBackground.classList.remove('dark-mode-calc-container');
+		numbersColor.classList.remove('dark-mode-numbers-upper');
+		upperBackground.classList.remove('dark-mode-upper-background-color')
+		lowerNumColor.forEach((num)=>{
+			num.classList.remove('dark-mode-lower-numbers-colors')
+		})
+
+		lowerOpertators.forEach((op)=>{
+			op.classList.remove('dark-mode-operator-color');
+		})
+
+		lowerOpertatorsCircle.forEach((circle)=>{
+			circle.classList.remove('dark-mode-operator-span');
+		})
+
+	}else if(toggle.checked === true){
+		calcBackground.classList.add('dark-mode-calc-container');
+		numbersColor.classList.add('dark-mode-numbers-upper');
+		upperBackground.classList.add('dark-mode-upper-background-color')
+		lowerNumColor.forEach((num)=>{
+			num.classList.add('dark-mode-lower-numbers-colors')
+		})
+
+		allBtns.forEach((button)=>{
+			button.addEventListener("mousedown",function(){
+				button.style.cssText = 'background-color:#4c5867; transform:scale(.9);'
+			})
+			button.addEventListener("mouseup",function(){
+				button.style.cssText = 'background-color:none;'
+			})
+		})
+
+
+		copyBtn.addEventListener('mousedown',function(){
+			copyBtn.style.cssText = 'background-color:#4c5867; transform:scale(1.1); transition:.1s ease-in-out;';
+			copyMsg.classList.add('appear-msg');
+			
+			setTimeout(function(){
+				copyMsg.classList.remove('appear-msg');
+			},3000)
+		})
+
+		copyBtn.addEventListener('mouseup',function(){
+			copyBtn.style.cssText = 'background-color:none;'
+		})
+
+		deleteBtn.addEventListener('mousedown',function(){
+			deleteBtn.style.cssText = 'background-color:#4c5867; transform:scale(1.1); transition:.1s ease-in-out;'
+		})
+
+		deleteBtn.addEventListener('mouseup',function(){
+			deleteBtn.style.cssText = 'background-color:none;'
+		})
+
+		
+		lowerOpertators.forEach((op)=>{
+			op.classList.add('dark-mode-operator-color');
+		})
+
+		lowerOpertatorsCircle.forEach((circle)=>{
+			circle.classList.add('dark-mode-operator-span');
+		})
+		
+	}
+
+})
+
+copyBtn.addEventListener('mousedown',function(){
+	copyBtn.style.cssText = 'background-color:#EEEEEE; transform:scale(1.1); transition:.1s ease-in-out;';
+	copyMsg.classList.add('appear-msg');
+	setTimeout(function(){
+		copyMsg.classList.remove('appear-msg');
+	},3000)
+})
+
+copyBtn.addEventListener('mouseup',function(){
+	copyBtn.style.cssText = 'background-color:none;'
+})
+
+deleteBtn.addEventListener('mousedown',function(){
+	deleteBtn.style.cssText = 'background-color:#EEEEEE; transform:scale(1.1); transition:.1s ease-in-out;'
+})
+
+deleteBtn.addEventListener('mouseup',function(){
+	deleteBtn.style.cssText = 'background-color:none;'
+})
+
 //add to all buttons when clicked they will get the style in css
 allBtns.forEach((button)=>{
 	button.addEventListener("mousedown",function(){
 		button.style.cssText = 'background-color:#EEEEEE; transform:scale(.9);'
 	})
 	button.addEventListener("mouseup",function(){
-		button.style.cssText = 'background-color:white;'
+		button.style.cssText = 'background-color:none;'
 	})
 })
 
@@ -180,6 +296,7 @@ window.addEventListener('keydown',function(e){
 				display.textContent = storing;
 		}
 	}
+
 	if(e.key === '0'){
 		if(storing.length >= 14){
 		one.removeEventListener('click',function(){});
@@ -192,16 +309,17 @@ window.addEventListener('keydown',function(e){
 				display.textContent = storing;
 		}
 	}
+
 	if(e.key === 'Backspace'){
 		storing = storing.slice(0,-1);
 		display.textContent = storing;
 
 	}
+
 	if(e.key === 'c'){
 		empty();
 		
 	}
-	
 
 	if(e.key === '='){
 	
@@ -285,6 +403,21 @@ window.addEventListener('keydown',function(e){
 			storing = '';
 			display.textContent = firstNum;
 			operator = '+';
+		}
+		pointCounter=0;
+	}
+
+	if(e.key === "%"){
+		if(operator === ''){
+			firstNum = +storing;
+			storing = '';
+			display.textContent = '';
+			operator = '%';
+		}else{
+			firstNum = operate(firstNum,operator,+storing);
+			storing = '';
+			display.textContent = firstNum;
+			operator = '%';
 		}
 		pointCounter=0;
 	}
@@ -437,14 +570,50 @@ zero.addEventListener('click',function(){
 
 })
 
-clear.addEventListener('click',function(){
-	empty();
-})
-
 deleteBtn.addEventListener('click',function(){
 	storing = storing.slice(0,-1);
 	display.textContent = storing;
 })
+
+copy.addEventListener('click',function(){
+	let textArea = document.createElement('textarea');
+	textArea.value = display.textContent;
+	if(display.textContent === ''){
+		copyMsg.textContent = 'There is nothing to copy';
+	}else{
+		document.body.appendChild(textArea);
+		textArea.select();
+		document.execCommand("Copy");
+		textArea.remove();
+		copyMsg.textContent = 'Copied to Clipboard';
+	}
+})
+
+clear.addEventListener('click',function(){
+	empty();
+})
+
+
+percent.addEventListener('click',function(){
+	if(operator === ''){
+		firstNum = +storing;
+		storing = '';
+		display.textContent = '';
+		operator = '%';
+	}else{
+		firstNum = operate(firstNum,operator,+storing);
+		storing = '';
+		display.textContent = firstNum;
+		operator = '%';
+	}
+	pointCounter=0;
+})
+
+negative.addEventListener('click',function(){
+	storing = +storing * (-1);
+	display.textContent = storing;
+})
+
 
 plus.addEventListener('click',function(){
 	
